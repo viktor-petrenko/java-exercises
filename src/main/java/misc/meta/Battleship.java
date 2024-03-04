f56ef45f6d412c9f914895513165d255ad30fe3a
ConsciousPen <109042349+ConsciousPen@users.noreply.github.com>
2024-03-04 01:10:14 -0600

    Battleship.java meta

--- src/main/java/misc/meta/Battleship.java
+++ src/main/java/misc/meta/Battleship.java
@@ -5,23 +5,21 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Battleship {
     public static void main(String[] args) {
-        // Write any import statements here
         int rows = 5;
         int columns = 5;
-
         int[][] matrix = new int[rows][columns];
 
-
-        for (int[] row: matrix)
-            Arrays.fill(row,  ThreadLocalRandom.current().nextInt(0, 2));
-
-        System.out.println(Arrays.deepToString(matrix));
-
         System.out.println(getHitProbability(rows,columns, matrix));
-
     }
 
     public static double getHitProbability(int R, int C, int[][] G) {
+        // Write any import statements here
+
+        for (int[] row: G)
+            Arrays.fill(row,  ThreadLocalRandom.current().nextInt(0, 2));
+
+        System.out.println(Arrays.deepToString(G));
+
         double ships = 0;
         for (int[] row : G) {
             for (int val : row) {  // Visit each cell in the row
