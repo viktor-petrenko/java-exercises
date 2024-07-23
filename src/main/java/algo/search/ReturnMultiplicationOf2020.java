9d2d9e261c7d6b0e889c8f747fec7921f96fd708
ConsciousPen <109042349+ConsciousPen@users.noreply.github.com>
2024-07-23 15:35:47 -0500

    ReturnMultiplicationOf2020.java

---
+++ src/main/java/algo/search/ReturnMultiplicationOf2020.java
@@ -0,0 +1,29 @@
+package algo.search;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReturnMultiplicationOf2020 {
+    public static void main(String[] args) {
+        List<Integer> numbersList = Arrays.asList(1721, 979, 366, 299, 675, 1456);
+        System.out.println(find2020sum(numbersList));
+    }
+
+    // let numbersArray = [1721, 979, 366, 299, 675, 1456];
+    public static Integer find2020sum(List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+            for (int n = 0; i < list.size(); i++) {
+                System.out.println("inner loop i '" + list.get(i) + "'");
+                System.out.println("inner loop n '" + list.get(n) + "'");
+                int result = list.get(n) + list.get(i);
+                if (result == 2020) {
+                    System.out.println("Result " + " " + list.get(n) + " + " + list.get(i) + "=" + result);
+                    System.out.println("Result " + " " + list.get(n) + " * " + list.get(i) + "=" + list.get(n) * list.get(i));
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+}
