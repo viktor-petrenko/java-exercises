fc6e2d9eb9e3a8cbd62daf895fd8147b50f1fd98
ConsciousPen <109042349+ConsciousPen@users.noreply.github.com>
2024-11-07 16:02:58 -0600

    v2

--- src/main/java/RemoveDuplicatesFromTheArray.java
+++ src/main/java/RemoveDuplicatesFromTheArray.java
@@ -1,11 +1,25 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class RemoveDuplicatesFromTheArray {
     public static void main(String[] args) {
 
         Integer[] arrayStorage = {1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 77, 77, 88, 99, 10, 11};
+        justUsingList(arrayStorage);
+
+        HashSet<Integer> setStorage = new HashSet<>(Arrays.asList(arrayStorage));
+
+        for (Integer setItem : setStorage) {
+            if (setItem % 2 == 0) {
+                System.out.println(true + " item '" + setStorage);
+            } else {
+                System.out.println(false + " item '" + setStorage);
+            }
+        }
+
+        System.out.println(setStorage);
+    }
+
+    private static void justUsingList(Integer[] arrayStorage) {
         List<Integer> listStorage = Arrays.stream(arrayStorage).toList().stream().sorted().toList();
 
         ArrayList<Integer> noDuplicates = new ArrayList<>();
