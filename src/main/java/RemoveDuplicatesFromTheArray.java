d5d86e2865023be15c484b383024a74a77b89171
ConsciousPen <109042349+ConsciousPen@users.noreply.github.com>
2024-11-07 14:08:59 -0600

    FindMinCostBasedOnProvidedData GenerateUniqueNameIfRepetitiveWordsFound LongestSubstringWithTwoDistinctChars

---
+++ src/main/java/RemoveDuplicatesFromTheArray.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RemoveDuplicatesFromTheArray {
+    public static void main(String[] args) {
+
+        Integer[] arrayStorage = {1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 77, 77, 88, 99, 10, 11};
+        List<Integer> listStorage = Arrays.stream(arrayStorage).toList().stream().sorted().toList();
+
+        ArrayList<Integer> noDuplicates = new ArrayList<>();
+
+        for (int i = 0; i < listStorage.size(); i++) {
+            if (i % 2 == 0) {
+                System.out.println(true + " item '" + listStorage.get(i));
+            } else {
+                System.out.println(false + " item '" + listStorage.get(i));
+            }
+            if (!noDuplicates.contains(listStorage.get(i))) {
+                noDuplicates.add(listStorage.get(i));
+            }
+        }
+        System.out.println(noDuplicates);
+    }
+}
